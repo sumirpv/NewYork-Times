b83b0ddc076520ee42188132a209eaa0bc77b65b
David <dhahn123@gmail.com>
2018-02-27 14:01:16 -0800

    updated version of js script

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,70 +1,34 @@
+$("button").on("click", function (event) {;
+    event.preventdefault();
+    var person = $("#exampleInputName2").val().trim();
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json"; +
+    person + "&api_key=cd70df70e6164680ae56df5cee60ca8f";
+    console.log(queryURL);
 
-
-        $("button").on("click", function() {
-         
-var person = $(this).attr("data-person");
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    person + "&api_key=cd70df70e6164680ae56df5cee60ca8f"    
-    
-      var queryURL = ;
-
-      //ajax function for api key
-      $.ajax({
+    //ajax function for api key
+    $.ajax({
         url: queryURL,
         method: "GET"
-      })
+    })
 
-      //function for the articles to be pulled
-      .then(function(response) {
+        //function for the articles to be pulled
+        .then(function (response) {
 
-    
-        var retrieveArticle = response.data; //object storing array of data
 
-        $("insert div here for articles to appear").empty(); //clears any repeating articles upon further searches
+            var retrieveArticle = response.data; //object storing array of data
 
-        var itemsList = $("<div>"); //creates a div on memory
-        itemsList.attr("tag used to modify", response.);
+            $("insert div here for articles to appear").empty(); //clears any repeating articles upon further searches
 
-        itemsList.prepend()
+            var itemsList = $("<div>"); //creates a div on memory
+            itemsList.attr("tag used to modify", response);
 
-        $("insert div here for articles to appear").prepend()
+            itemsList.prepend()
 
-      
+            $("insert div here for articles to appear").prepend()
 
-  
-        $("button").on("click", function.() {
-         
-var person = $(this).attr("data-person");
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    person + "&api_key=cd70df70e6164680ae56df5cee60ca8f"    
-    
-      var queryURL = ;
 
-      //ajax function for api key
-      $.ajax({
-        url: queryURL,
-        method: "GET"
-      })
 
-      //function for the articles to be pulled
-      .then(function(response) {
+                    });
 
-    
-        var retrieveArticle = response.data; //object storing array of data
+            });
 
-        $("insert div here for articles to appear").empty(); //clears any repeating articles upon further searches
-
-        var itemsList = $("<div>"); //creates a div on memory (exists only in JS thus far)
-        itemsList.attr("tag used to modify", response.);
-
-        itemsList.prepend()
-
-        $("insert div here for articles to appear").prepend()
-
-      
-        
-      });
-        
-      });
-
-   
